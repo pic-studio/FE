cf213d2c359b1c7ee29004ea3af932c7ad6e7982
SoYounHo <sojaeheung@sojaeheung-ui-MacBookPro.local>
2022-09-28 16:16:09 +0900

    style: SpeechBubble 컴포넌트 수정

--- src/components/SpeechBubble.tsx
+++ src/components/SpeechBubble.tsx
@@ -12,16 +12,15 @@ const SpeechBubble = ({githubLink, blogLink, children}: SpeechBubbleProps) => {
       <h3>position: {children}</h3>
       <div className="TeamLink">
         <ul>
-          <li><a href={githubLink}>GitHun</a></li>
-          <li><a href={blogLink}>Blog</a></li>
+          <li><a href={githubLink} target={"_blank"}>GitHun</a></li>
+          <li><a href={blogLink} target={"_blank"}>Blog</a></li>
         </ul>
       </div>
-
     </SpeechBubbleComponents>
   )
 };
 
-const SpeechBubbleComponents =  styled.div`
+const SpeechBubbleComponents = styled.div`
   min-width: 169px;
   height: 62px;
   position: absolute;
@@ -31,6 +30,13 @@ const SpeechBubbleComponents =  styled.div`
   border-radius: 10px;
   z-index: 9;
   transition: all 0.2s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .TeamLink ul {margin-top:8px;}
+  .TeamLink ul li {display: inline-block;}
+  .TeamLink ul li:first-child {margin-right: 10px;}
+
 
   ${({theme}) => {
     const {fontSizes, fontWeight, colors } = theme
